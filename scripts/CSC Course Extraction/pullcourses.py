497040bd0ad78395dfcd5629a13d9011111ace15
John M <john@stolen-bikes.co.uk>
2016-04-05 18:27:38 -0400

    Changes to be committed: 	new file:   CSC Course Extraction/output.csv 	new file:   CSC Course Extraction/pullcourses.py

---
+++ scripts/CSC Course Extraction/pullcourses.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+import sys, os, argparse, urllib2, time, json
+from urllib2 import Request
+from multiprocessing import Pool
+
+domain = 'https://pod.cybersecuritychallenge.org.uk'
+page = '/lib/ajax/getnavbranch.php'
+idrange = range(1, 200)
+typenum = 10
+output = 'output.csv'
+
+fHandle = open(output,'a')
+fHandle.write('\"categoryName\"' + ', ' + '\"categorylink\"' + ', ' + '\"courseName\"' + ', ' + '\"courseType\"' + ', ' + '\"courseLink\"' + ', ' + '\"courseHidden\"' + '\n')
+fHandle.close()
+
+for idnum in idrange:
+	time.sleep(1) # Be nice lets not clog up CSC
+	try:
+		request = Request(domain + page + '?id=' + str(idnum) + '&type='+str(typenum))
+		request = urllib2.urlopen(request)
+		response = request.read()
+		#Covert response to JSON so we can process it
+		
+	except urllib2.HTTPError:
+		#invalid category so 500 error
+		print 'N'
+		continue
+
+	#process response to json
+	try:
+		jsonresponse = json.loads(response);
+	except ValueError:
+		#It wasnt json
+		print 'N'
+		continue
+
+	# Lets now try to process the json
+	#No point doing this if it's an empty category
+	if 'children' in jsonresponse:
+
+		categoryname = jsonresponse['name']
+		categorylink = jsonresponse['link']
+
+		for child in jsonresponse['children']:
+			if child['type'] == 20:
+				#print categoryname
+				#print categorylink
+				#print child['name']
+				#print child['type']
+				#print child['link']
+				#print child['hidden']
+				fHandle = open(output,'a')
+				fHandle.write('\"' + categoryname + '\", \"' + categorylink + '\", \"' + child['name'] + '\", \"' + str(child['type']) + '\", \"' + child['link'] + '\", \"' + str(child['hidden']) + '\"\n')
+				fHandle.close()
+				print 'C'
+				
+	
+
