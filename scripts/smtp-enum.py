79a3e01aee1aaac9c808fff689404267d15376bf
John Moss <john@stolen-bikes.co.uk>
2016-05-07 18:48:02 +0100

    Update smtp-enum.py

--- scripts/smtp-enum.py
+++ scripts/smtp-enum.py
@@ -3,15 +3,15 @@
 import socket
 import sys
 
-if len(sys.argv) != 2:
-	print "Usage smtp-enum.py <username>"
+if len(sys.argv) != 3:
+	print "Usage smtp-enum.py <target> <username>"
 	sys.exit(0)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-connect=s.connect(('10.11.1.115', 25))
+connect=s.connect((sys.argv[1], 25))
 banner=s.recv(1024)
 print banner
-s.send('VRFY ' + sys.argv[1] + '\r\n')
+s.send('VRFY ' + sys.argv[2] + '\r\n')
 result=s.recv(1024)
 print result
 s.close()
